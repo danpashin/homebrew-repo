f536396a441c7052cc3258915d52ad7277cd82ac
Dan Pashin <danpashin@pm.me>
2022-05-28 22:36:34 +0300

    [+] Add symlinks

---
+++ Formula/symlinks.rb
@@ -0,0 +1,13 @@
+class Symlinks < Formula
+  desc "Symbolic link maintenance utility"
+  homepage "https://github.com/brandt/symlinks"
+  url "https://github.com/brandt/symlinks/archive/e22cde5c1c5ce5ebef0173993dc67e3e9c811c33.tar.gz"
+  version "1.0"
+  sha256 "ac00a5a0b5df8a9166531cf5e988698bdcc515024b61a862dc32d0be485ba9a8"
+  head "https://github.com/brandt/symlinks.git", branch: "master"
+
+  def install
+    system ENV.cc.to_s, "-o", "symlinks", "-O3", "symlinks.c"
+    bin.install "symlinks"
+  end
+end
