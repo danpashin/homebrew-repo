da0a42dbdfc777a3894b80dff1700292c37ba47e
Dan Pashin <danpashin@pm.me>
2022-06-04 21:45:52 +0300

    [+] Add option to untrunc to build with gui

--- Formula/untrunc.rb
+++ Formula/untrunc.rb
@@ -1,15 +1,22 @@
-class Untruc < Formula
-  desc "NewNode decentralized Content Distribution Network"
+class Untrunc < Formula
+  desc "Restore a truncated mp4/mov. Improved version of ponchio/untrunc"
   homepage "https://github.com/anthwlock/untrunc"
   url "https://github.com/anthwlock/untrunc/archive/d72ec324fbc29eb00b53c7dafeef09f92308962b.tar.gz"
   version "0.1.0"
   sha256 "d8a35992e1f4375ce94048c23cdf3d7034396123da253275d30983ab6884b559"
   head "https://github.com/anthwlock/untrunc.git", branch: "master"
 
+  option "with-gui", "Compile with GUI support"
   depends_on "ffmpeg@4"
+  depends_on "libui" => :optional
 
   def install
     system "make"
     bin.install "untrunc"
+
+    if build.with? "gui"
+      system "make", "untrunc-gui"
+      bin.install "untrunc-gui"
+    end
   end
 end
