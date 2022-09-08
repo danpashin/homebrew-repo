f6ef4edc56cf46ffa55c54c58d550de948a5eb93
Dan Pashin <danpashin@pm.me>
2022-09-08 09:21:24 +0300

    Update libzim

--- Formula/zim-tools.rb
+++ Formula/zim-tools.rb
@@ -1,9 +1,9 @@
 class ZimTools < Formula
   desc "Various ZIM command-line tools"
   homepage "https://github.com/openzim/zim-tools"
-  version "3.1.1"
+  version "3.1.2"
   url "https://github.com/openzim/zim-tools/archive/refs/tags/#{version}.tar.gz"
-  sha256 "980352ded328cadb41fce43706b679065b4933169d3909e82ea52b07e2cc18d5"
+  sha256 "93af4a314d02d87facf5c24b9f595937db5ebf1563d22728dc8cc434b23d89c5"
   license "GPL-3.0-or-later"
   head "https://github.com/openzim/zim-tools.git", branch: "master"
 
