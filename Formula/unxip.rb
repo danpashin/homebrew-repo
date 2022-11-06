b3c58acf951372c5a33df538d2228fc8e5baef41
Dan Pashin <danpashin@pm.me>
2022-11-06 12:35:20 +0300

    Update unxip

--- Formula/unxip.rb
+++ Formula/unxip.rb
@@ -1,9 +1,9 @@
 class Unxip < Formula
   desc "Fast Xcode unarchiver"
   homepage "https://github.com/saagarjha/unxip"
-  version "1.1.1"
+  version "1.1.3"
   url "https://github.com/saagarjha/unxip/releases/download/v#{version}/unxip"
-  sha256 "76cc29b590d22487d382c662e38aeb558f3fef0449edeb85ff2e404904fc5ffd"
+  sha256 "ad0755a3b01940e11e0a2c5a3d9b6721f8aeb598145fc96a40c9ec2bbc25f54a"
   license "GPL-3.0"
 
   def install
