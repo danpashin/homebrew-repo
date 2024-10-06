8afffa96083d6d8af92a97f1d8e043ca5c36a54d
Dan Pashin <danypashin@yandex.ru>
2024-10-06 14:43:47 +0300

    update untrunc

--- Formula/untrunc.rb
+++ Formula/untrunc.rb
@@ -1,15 +1,19 @@
 class Untrunc < Formula
   desc "Restore a truncated mp4/mov. Improved version of ponchio/untrunc"
   homepage "https://github.com/anthwlock/untrunc"
-  url "https://github.com/anthwlock/untrunc/archive/d72ec324fbc29eb00b53c7dafeef09f92308962b.tar.gz"
+  url "https://github.com/anthwlock/untrunc/archive/13cafedf59369db478af537fb909f0d7fd0eb85f.tar.gz"
   version "0.1.0"
-  sha256 "d8a35992e1f4375ce94048c23cdf3d7034396123da253275d30983ab6884b559"
+  sha256 "dde668893161ef82241b1ad87405be299786cca39f2a7482ffd95fa1652a19a3"
   license :public_domain
   head "https://github.com/anthwlock/untrunc.git", branch: "master"
 
   option "with-gui", "Compile with GUI support"
-  depends_on "ffmpeg@4"
+  depends_on "ffmpeg@6"
   depends_on "libui" if build.with? "gui"
+
+  on_linux do
+    depends_on "make" => :build
+  end
 
   def install
     system "make"
